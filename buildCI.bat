863ade02b84a40daf388f55db65adf89ccb1bab3
Lukas Snelinski <lsnelinski@icloud.com>
2022-01-07 17:01:34 +0100

    added two .bat files for solution build

---
+++ buildCI.bat
@@ -0,0 +1,12 @@
+:: Variables
+set pathToSolution="%cd%\FileHashGenerator.sln"
+set pathToMSBuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin"
+
+:: Restore NuGet
+nuget.exe restore
+
+:: Build Solution
+cd /d %pathToMSBuild%
+MSBuild.exe %pathToSolution% /p:configuration=debug
+
+pause
